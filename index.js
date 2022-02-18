9269412af571f73e464630fc13a50d6ec48778c5
Joel Sprouse <joel.sprouse11@gmail.com>
2022-02-18 11:12:29 +0000

    Removed unneeded spaces

--- index.js
+++ index.js
@@ -24,11 +24,8 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-
 	const command = client.commands.get(interaction.commandName);
-
 	if (!command) return;
-
 	try {
 		await command.execute(interaction);
 	} catch (error) {
